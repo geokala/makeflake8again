7a3002ee90dd3b0b855bb774739fd88ac25420cc
Madda <nobody.youknow@local>
2017-02-02 20:58:12 +0000

    Correct indentation check

--- flake8_again/__init__.py
+++ flake8_again/__init__.py
@@ -4,6 +4,9 @@ class Flake8Again():
     flake8ed = (
         'IFLK*ANG1 code should be better %s:%s'
     )
+    wrongindent = (
+        'FLK8AGN1 line indented with only tabs or only spaces. Recommended to use both.'
+    )
     def __init__(self, tree, filename, line_number, logical_line, lines):
         self.filename = filename
         self.line_number = line_number
@@ -18,3 +21,17 @@ class Flake8Again():
         for line in range(0, len(self.lines)):
             # yield line_num, column, message, type?
             yield 666, 80, self.flake8ed % (line, self.lines[line].rstrip()), type(None)
+
+            line_number = line
+            line = self.lines[line]
+            tabs_found = False
+            spaces_found = False
+            for char in line:
+                if char not in (' ', '\t'):
+                    break
+                elif char == ' ':
+                    spaces_found = True
+                elif char == '\t':
+                    tabs_found = True
+
+            yield line_number, 4, self.wrongindent, type(None)
