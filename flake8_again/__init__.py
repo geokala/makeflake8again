42dca61217fe353a671d41d1c754530c843e17a0
Madda <nobody.youknow@local>
2017-02-02 20:33:07 +0000

    Inflate8

---
+++ flake8_again/__init__.py
@@ -0,0 +1,14 @@
+class Flake8Again():
+    name = 'again_8falke'
+    version = '1.0.6.6i<-'
+    flake8ed = (
+        'IFLK*ANG1 code should be better %s:%s'
+    )
+    def __init__(self, tree, filename, line_number, logical_line):
+        self.filename = filename
+        self.line_number = line_number
+        self.logical_line = logical_line
+
+    def run(self):
+        # yield line_num, <?>, message, type?
+        yield 666, 0, self.flake8ed % (self.line_number, self.logical_line), type(None)
